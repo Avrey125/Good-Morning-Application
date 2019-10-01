4deb7831060f71244850be81fad2fc0856f9358c
cdcummings10 <ccummings0913@gmail.com>
2019-10-01 11:14:29 -0700

    merging to development2

---
+++ public/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function storeUserInfo(e){
+  
+}
+
+function renderCalendar(){
+  let gmail;
+
+  $('#displayRight').append(`<iframe src="https://calendar.google.com/calendar/embed?src=${gmail}%40gmail.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>`)
+}
